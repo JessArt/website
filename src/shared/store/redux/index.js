a499a8054342b3331927581ad68d9703fba7af86
Seva Zaikov <seva.zaikov@gmail.com>
2017-12-28 02:18:55 +0100

    update store tiles

--- src/shared/store/redux/index.js
+++ src/shared/store/redux/index.js
@@ -1,22 +1,26 @@
 /* global __DEV__ */
 
-import { createStore, applyMiddleware } from 'redux'
-import { createMiddleware, createEntities } from 'redux-tiles'
-import tiles from './tiles'
-import * as api from '../../utils/fetch'
-import logger from 'redux-logger'
+import { createStore, applyMiddleware } from "redux";
+import { createMiddleware, createEntities } from "redux-tiles";
+import tiles from "./tiles";
+import * as api from "../../utils/fetch";
+import logger from "redux-logger";
 
-const { actions, reducer, selectors } = createEntities(tiles)
+const { actions, reducer, selectors } = createEntities(tiles);
 export default function createStoreInstance({ initialState = {} } = {}) {
-  const { middleware, waitTiles } = createMiddleware({ actions, selectors, api })
-  const middlewares = [middleware].concat(__DEV__ ? logger : [])
+  const { middleware, waitTiles } = createMiddleware({
+    actions,
+    selectors,
+    api
+  });
+  const middlewares = [middleware].concat(__DEV__ ? logger : []);
   const store = createStore(
     reducer,
     initialState,
     applyMiddleware(...middlewares)
-  )
+  );
 
-  return { store, waitTiles }
+  return { store, waitTiles };
 }
 
-export { actions, selectors }
+export { actions, selectors };
--- src/shared/store/redux/tiles/media.js
+++ src/shared/store/redux/tiles/media.js
@@ -1,29 +1,74 @@
-import { createTile } from 'redux-tiles'
+import { createTile } from "redux-tiles";
 
 export const images = createTile({
-  type: ['api', 'images'],
-  fn: ({ api, params }) => api.get('/v2/api/images', { params }),
-  nesting: ({ type }) => [type],
+  type: ["api", "images"],
+  fn: async ({ api, params }) =>
+    api.get("/v1/images", { params: { pageSize: 20, ...params } }),
+  nesting: ({ type, tags = ["default"], pageNumber = 0, pageSize = 20 }) => [
+    type,
+    tags.join(""),
+    pageNumber,
+    pageSize
+  ],
   caching: true
-})
+});
+
+export const image = createTile({
+  type: ["api", "image"],
+  fn: async ({ api, params }) => api.get(`/v1/images/${params.id}`),
+  nesting: ({ id }) => [id],
+  caching: true
+});
 
 export const articles = createTile({
-  type: ['api', 'articles'],
-  fn: ({ api }) => api.get('/v1/api/articles'),
+  type: ["api", "articles"],
+  fn: ({ api }) => api.get("/v1/articles"),
   caching: true
-})
+});
+
+export const tumblr = createTile({
+  type: ["api", "tumblr"],
+  fn: ({ api }) => api.get("/v1/progress")
+});
 
 export const stories = createTile({
-  type: ['api', 'stories'],
-  fn: ({ api }) => api.get('/v1/api/stories'),
+  type: ["api", "stories"],
+  fn: ({ api }) => api.get("/v1/api/stories"),
   caching: true
-})
+});
 
 export const story = createTile({
-  type: ['api', 'story'],
+  type: ["api", "story"],
   fn: ({ api, params }) => api.get(`/v1/api/stories/${params.id}`),
   nesting: ({ id }) => [id],
   caching: true
-})
+});
 
-export default [images, articles, stories, story]
+export const topTags = createTile({
+  type: ["api", "topTags"],
+  fn: ({ api, params }) =>
+    api.get("/v1/top_tags", { params: { ...params, nesting: 2 } }),
+  nesting: ({ type }) => [type]
+});
+
+export const feedback = createTile({
+  type: ["api", "feedback"],
+  fn: ({ api, params }) => api.post("/v1/feedback", { params })
+});
+
+export const subscribe = createTile({
+  type: ["api", "subscribe"],
+  fn: ({ api, params }) => api.post("/v1/subscribe", { params })
+});
+
+export default [
+  images,
+  articles,
+  stories,
+  story,
+  topTags,
+  tumblr,
+  image,
+  subscribe,
+  feedback
+];
