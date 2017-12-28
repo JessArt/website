b05941ee4ab6899257bf594a754f0812848a7257
Seva Zaikov <seva.zaikov@gmail.com>
2017-12-28 02:19:14 +0100

    switch to the new NODE.js API

--- src/shared/utils/fetch.js
+++ src/shared/utils/fetch.js
@@ -1,62 +1,66 @@
-import isNode from 'detect-node'
+import isNode from "detect-node";
 
-const API = 'cms.jess.gallery'
+const API = "node-api.jess.gallery";
 
 function constructPrefix() {
-  const prefix = isNode ? 'http:' : location.protocol
+  const prefix = isNode ? "http:" : location.protocol;
 
-  return `${prefix}//${API}`
+  return `${prefix}//${API}`;
 }
 
 function augmentURL(url, params) {
-  const queryParams = params ? Object
-    .keys(params)
-    .map(key => {
-      const value = params[key]
-      return `${key}=${encodeURIComponent(value)}`
-    })
-    .join('&') : ''
+  const queryParams = params
+    ? Object.keys(params)
+        .map(key => {
+          const value = params[key];
+          if (value) {
+            return `${key}=${encodeURIComponent(value)}`;
+          }
+        })
+        .filter(Boolean)
+        .join("&")
+    : "";
 
-  const prefix = constructPrefix()
+  const prefix = constructPrefix();
 
-  return `${prefix}${url}${queryParams.length ? '?' :''}${queryParams}`
+  return `${prefix}${url}${queryParams.length ? "?" : ""}${queryParams}`;
 }
 
 function checkStatus(response) {
   if (response.status >= 200 && response.status < 300) {
-    return response
+    return response;
   } else {
-    var error = new Error(response.statusText)
-    error.response = response
-    throw error
+    var error = new Error(response.statusText);
+    error.response = response;
+    throw error;
   }
 }
 
 function parseJSON(response) {
-  return response.json()
+  return response.json();
 }
 
 export function get(url, { params } = {}) {
   return fetch(augmentURL(url, params), {
-    method: 'GET',
+    method: "GET",
     headers: {
-      'Accept': 'application/json',
-      'Content-Type': 'application/json'
+      Accept: "application/json",
+      "Content-Type": "application/json"
     }
   })
-  .then(checkStatus)
-  .then(parseJSON)
+    .then(checkStatus)
+    .then(parseJSON);
 }
 
 export function post(url, { params }) {
   return fetch(`${constructPrefix()}${url}`, {
-    method: 'POST',
+    method: "POST",
     headers: {
-      'Accept': 'application/json',
-      'Content-Type': 'application/json'
+      Accept: "application/json",
+      "Content-Type": "application/json"
     },
     body: JSON.stringify(params)
   })
-  .then(checkStatus)
-  .then(parseJSON)
+    .then(checkStatus)
+    .then(parseJSON);
 }
